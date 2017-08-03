e0e96780040a53c9e8708f3634eea28e20abd1cd
jenniferwiner <jeniwiner@gmail.com>
2017-08-03 13:20:57 -0600

    add comments

--- src/App.js
+++ src/App.js
@@ -169,10 +169,10 @@ class App extends Component {
     let totalExpense = this.state.oneTimeExpense + (this.state.monthlyExpense * 12)
     let monthlyContributionProfit = this.state.monthlyRevenue - this.state.monthlyExpense
     let totalContributionProfit = totalRevenue - totalExpense
-
+    // handle case where totalRevenue is 0 (to avoid -Infinity and NaN)
     let contributionMargin = totalRevenue !== 0 ? (totalContributionProfit / totalRevenue * 100).toFixed(0) : 0
-
-    let capitalROI = (totalRevenue === 0 && totalExpense === 0) ? 0 : ((this.state.oneTimeExpense - this.state.oneTimeRevenue) / monthlyContributionProfit).toFixed(1)
+    // handle case where totalExpense is 0 (to avoid NaN)
+    let capitalROI = (totalExpense === 0) ? 0 : ((this.state.oneTimeExpense - this.state.oneTimeRevenue) / monthlyContributionProfit).toFixed(1)
 
     return (
       <div>
